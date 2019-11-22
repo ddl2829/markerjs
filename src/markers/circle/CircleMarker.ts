7daffdb85e07608dd10b1b3ff3a49444a6a3e2b2
Dale Smith <dale@dalesmithwebdev.com>
2019-11-22 12:36:46 -0500

    Make ellipse track the handles better

--- src/markers/circle/CircleMarker.ts
+++ src/markers/circle/CircleMarker.ts
@@ -13,6 +13,9 @@ export class CircleMarker extends RectangularMarkerBase {
     protected setup() {
         super.setup();
         this.markerCircle = SvgHelper.createEllipse(this.width / 2, this.height / 2);
+        this.markerCircle.setAttribute("fill", "rgba(0,0,0,0)");
+        this.markerCircle.setAttribute("stroke", "rgb(255,0,0)");
+        this.markerCircle.setAttribute("stroke-width", "3");
         this.addToRenderVisual(this.markerCircle);
         SvgHelper.setAttributes(this.visual, [["class", "circle-marker"]]);
     }
@@ -21,6 +24,10 @@ export class CircleMarker extends RectangularMarkerBase {
         super.resize(x, y);
         this.markerCircle.setAttribute("rx", (this.width / 2).toString());
         this.markerCircle.setAttribute("ry", (this.height / 2).toString());
+        this.markerCircle.setAttribute("cx", (this.width / 2).toString());
+        this.markerCircle.setAttribute("cy", (this.height / 2).toString());
     }
+
+    //<ellipse fill="rgba(0,0,0,0)" stroke="rgb(255,0,0)" stroke-width="3" cx="100" cy="25" rx="93" ry="46"></ellipse>
 
 }
