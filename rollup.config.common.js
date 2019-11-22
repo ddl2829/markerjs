bf142e07be76182b999837c10e97844d2f4fce43
Dale Smith <dale@dalesmithwebdev.com>
2019-11-22 11:58:56 -0500

    Fixes structure?

--- rollup.config.common.js
+++ rollup.config.common.js
@@ -3,9 +3,9 @@ import del from 'rollup-plugin-delete';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 import svgo from 'rollup-plugin-svgo';
-import generatePackageJson from 'rollup-plugin-generate-package-json'
+import generatePackageJson from 'rollup-plugin-generate-package-json';
 
-const outputDir = "./dist/";
+const outputDir = './dist/';
 
 let leanPkg = Object.assign({}, pkg);
 leanPkg.scripts = {};
@@ -20,55 +20,57 @@ see README.md and LICENSE for details
 ********************************** */`;
 
 export default [
-    {
-        input: 'src/index.ts',
-        plugins: [
-            del({ targets: 'dist/*' }),
-            typescript({
-                clean: true,
-                useTsconfigDeclarationDir: true,
-            }),
-            postcss(),
-            svgo(),
-            generatePackageJson({
-                baseContents: leanPkg
-            })],
-        output: [
-            {
-                file: outputDir + pkg.module,
-                format: 'es',
-                banner: banner,
-            },
-            {
-                file: outputDir + pkg.main,
-                name: pkg.name,
-                format: 'umd',
-                sourcemap: true,
-                banner: banner,
-            },
-        ]
-    },
-    // workaround for terser plugin bug with multiple outputs
-    // https://github.com/TrySound/rollup-plugin-terser/issues/5
-    {
-        input: 'src/index.ts',
-        plugins: [
-            typescript({
-                clean: true,
-                useTsconfigDeclarationDir: true,
-            }),
-            postcss(),
-            svgo(),
-            generatePackageJson({
-                baseContents: leanPkg
-            })],
-        output: [
-            { 
-                file: outputDir + pkg.main, 
-                name: pkg.name, 
-                format: 'umd',
-                banner: banner
-            },
-        ]
-    }
+	{
+		input: 'src/index.ts',
+		plugins: [
+			del({ targets: 'dist/*' }),
+			typescript({
+				clean: true,
+				useTsconfigDeclarationDir: true
+			}),
+			postcss(),
+			svgo(),
+			generatePackageJson({
+				baseContents: leanPkg
+			})
+		],
+		output: [
+			{
+				file: outputDir + 'markerjs.esm.js',
+				format: 'es',
+				banner: banner
+			},
+			{
+				file: outputDir + 'markerjs.umd.js',
+				name: pkg.name,
+				format: 'umd',
+				sourcemap: true,
+				banner: banner
+			}
+		]
+	},
+	// workaround for terser plugin bug with multiple outputs
+	// https://github.com/TrySound/rollup-plugin-terser/issues/5
+	{
+		input: 'src/index.ts',
+		plugins: [
+			typescript({
+				clean: true,
+				useTsconfigDeclarationDir: true
+			}),
+			postcss(),
+			svgo(),
+			generatePackageJson({
+				baseContents: leanPkg
+			})
+		],
+		output: [
+			{
+				file: outputDir + 'markerjs.umd.js',
+				name: pkg.name,
+				format: 'umd',
+				banner: banner
+			}
+		]
+	}
 ];
